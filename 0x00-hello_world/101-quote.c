a62873f734a9ce45b01360fccccf2625dbbf5172
Desmond Karani <karanidesmond@proton.me>
2023-07-13 12:18:38 -0700

    nkt ugghh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - initiates execution.
+ *
+ * Return: Must return 1 for success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
