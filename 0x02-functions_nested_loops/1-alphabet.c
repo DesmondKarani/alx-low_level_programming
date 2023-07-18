93576fb3ab1a72422adc318b1fef280547754ba5
Desmond Karani <karanidesmond@proton.me>
2023-07-18 19:55:26 +0300

    refresh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,40 +1,15 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
+ * print_alphabet -  a function that prints the alphabet, in lowercase, followed by a new line.
  */
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
 
-/**
- * print_alphabet - function that prints the alphabet, in lowercase,
- *                 followed by a new line.
- */
 void print_alphabet(void)
 {
-	char c;
+	char letter;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-	_putchar(c);
-	_putchar(' ');
-	/*To print a space after each character*/
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
-}
-
-/**
- * main - Main function
- *
- * Return: 0 on success
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
