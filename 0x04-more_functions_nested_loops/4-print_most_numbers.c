79c36f69c9763ff48a83a326b74f6d8c5e73df2c
Desmond Karani <karanidesmond@proton.me>
2023-07-20 16:29:37 +0300

    betty rectify

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers, from 0 to 9, 
+ * print_most_numbers - Prints the numbers, from 0 to 9,
  * followed by a new line, but does not print 2 and 4.
  *
  * Return: Nothing.
@@ -9,7 +9,7 @@
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
