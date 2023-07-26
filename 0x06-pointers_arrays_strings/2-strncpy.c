463b7ee3548b025cc27ae16e97fb584deaa23974
Desmond Karani <karanidesmond@proton.me>
2023-07-26 22:28:01 +0300

    string copying

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strncpy -  a function that copies a string.
+ * @dest: Variable to receive the copied string.
+ * @src: variable to source the copy.
+ * @n: determiner for how many elements to copy from src variable.
+ *
+ * Return: the function must return dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	dest[n] = '\n';
+
+	return (dest);
+}
