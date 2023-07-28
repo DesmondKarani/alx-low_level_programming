2568f99e7e7039a5c527eeea1f47b80eeaa8704b
Desmond Karani <karanidesmond@proton.me>
2023-07-28 09:52:18 +0300

    correcting the main.h

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,11 +8,20 @@
  */
 char *string_toupper(char *d)
 {
+	/**
+	 * In this loop, I am iterating through each string character,
+	 *starting from 0, with d[b] making sure the index is not null '\0'
+	 */
 	for (int b = 0; d[b]; b++)
 	{
-		
+		/**
+		 * Here, the loop goes through the string starting from
+		 * element 0 changing them to uppercase.
+		 * Uses toupper function from ctype library.
+		 */
 		d[b] = toupper((unsigned char) d[b]);
 	}
 
+	/*Here I am just returning the modified string*/
 	return (d);
 }
