cfee31ac35851ad2ea46988509965f343166f179
Desmond Karani <karanidesmond@proton.me>
2023-07-28 14:12:14 +0300

    separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,8 @@
 /**
  * cap_string - a function that capitalizes all words of a string.
  * @sed: the string to be capitalized.
+ * is_separator - a function that checks if a character is one of
+ * the predefined separators.
  * des: the variable to index and use to loop through the string.
  * Return: the function returns sed, the converted string.
  */
@@ -9,6 +11,19 @@ char *cap_string(char *sed)
 {
 	/*Initializing the variable I will use in the loop*/
 	int des = 0;
+
+	/**
+	 * is_separator - a function that checks if a character is one of the
+	 * predefined separators.
+	 * @c: the character to be checked.
+	 * Return: 1 if the character is a separator, 0 otherwise.
+	 */
+	int is_separator(char c)
+	{
+		return (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' ||
+				c == '.' || c == '!' || c == '?' || c == '"' || c == '(' ||
+				c == ')' || c == '{' || c == '}');
+	}
 
 	/*if statement to capitalize first character if it is lowercase*/
 	if (sed[des] >= 'a' && sed[des] <= 'z')
@@ -22,7 +37,7 @@ char *cap_string(char *sed)
 	 */
 	for (des = 1; sed[des] != '\0'; des++)
 	{
-		if (sed[des - 1] == ' ' && sed[des] >= 'a' && sed[des] <= 'z')
+		if (is_separator(sed[des - 1]) && sed[des] >= 'a' && sed[des] <= 'z')
 		{
 			sed[des] = sed[des] - ('a' - 'A');
 		}
