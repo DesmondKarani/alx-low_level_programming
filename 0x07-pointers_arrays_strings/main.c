4906575ccf8d8dce5c90365ab0f6d01311eec695
Desmond Karani <karanidesmond@proton.me>
2023-07-31 21:42:05 +0300

    correction

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -2,55 +2,38 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
-
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char buffer[98] = {0x00};
+    char *s = "hello";
+    char *f;
 
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
     return (0);
 }
-char *_memset(char *s, char b, unsigned int n)
-{
-        unsigned int i;
 
-        for (i = 0; i < n; i++)
+char *_strchr(char *s, char c)
+{
+        while (*s != '\0')
         {
-                s[i] = b;
+                if (*s == c)
+                {
+                        return (s);
+                }
+                s++;
         }
-        return (s);
+        /*check if 'c' is the null character*/
+        if (*s == c)
+        {
+                return (s);
+        }
+        return (NULL);
 }
