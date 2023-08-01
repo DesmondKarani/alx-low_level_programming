445a4fe469bba035a27f22986ad50eb2275000bc
Desmond Karani <karanidesmond@proton.me>
2023-08-01 18:23:36 +0300

    for loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,16 @@
  * @s: The string in which to search for the character.
  * @c: The character to locate.
  *
- * Return: A pointer to the first occurrence of the character 'c' in
- *	the string 's', or NULL if the character is not found.
+ * Return: 0 signify success.
  */
 char *_strchr(char *s, char c)
 {
-	do {
-		if (*s == c)
-		{
-			return (s);
-		}
-	} while (*s++);
+	int i = 0;
 
-	return (NULL);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
