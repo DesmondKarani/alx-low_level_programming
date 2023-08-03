72c50a85fb12930ed010daf8a7fb85d51c241516
Desmond Karani <karanidesmond@proton.me>
2023-08-03 19:52:09 +0300

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_palindrome -  a function that returns 1 if a string is a palindrome
+ *	and 0 if not.
+ * @s: The string to be assessed.
+ * Return: Returns 1 if string is empty, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (0);
+}
