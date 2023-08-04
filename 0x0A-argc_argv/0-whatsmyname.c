9c26481fa480f9634e41c68f3609c4a2848a38c2
Desmond Karani <karanidesmond@proton.me>
2023-08-04 22:09:33 +0300

    program naming itself

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  main - Prints program name, followed by a new line.
+ *  @argc: Argument count.
+ *  @argv: Argument vector.
+ *
+ *  Return: returns 0 for success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+int main(int argc, char *argv[]);
+
+#endif /* MYLIB_H */
