51244d736a86a572d8f2a3a07dcf440c02dd0048
Desmond <karanidesmond@protonmail.ch>
2023-08-24 20:59:19 +0300

    header file edit

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LIST_H
+#define LIST_H
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -7,4 +7,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif /* LIST_H */
